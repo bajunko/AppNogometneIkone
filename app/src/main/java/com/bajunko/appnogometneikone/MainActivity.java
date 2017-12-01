2490aff081283db5b84aed9594acda189b734c4f
Ante Celic <bajunko@gmail.com>
2017-12-01 15:53:24 +0100

    Dodan a su tri TextView i na jedan od njih onClick metoda

--- app/src/main/java/com/bajunko/appnogometneikone/MainActivity.java
+++ app/src/main/java/com/bajunko/appnogometneikone/MainActivity.java
@@ -1,13 +1,24 @@
 package com.bajunko.appnogometneikone;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void onZvijezde(View view){
+
+        TextView zvijezde = (TextView) findViewById(R.id.textViewZvijezde);
+        Log.i("Tu sam", "Kliknu sam na Zvijezde");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
