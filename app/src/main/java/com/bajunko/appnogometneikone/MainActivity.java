d39b4b49652b93ec66c9cc3845d659589e185206
Ante Celic <bajunko@gmail.com>
2017-12-14 16:00:30 +0100

    dodani log ispis za button klubovi i kviz

--- app/src/main/java/com/bajunko/appnogometneikone/MainActivity.java
+++ app/src/main/java/com/bajunko/appnogometneikone/MainActivity.java
@@ -9,9 +9,18 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     public void onZvijezde(View view){
-
         TextView zvijezde = (TextView) findViewById(R.id.textViewZvijezde);
         Log.i("Tu sam", "Kliknu sam na Zvijezde");
+    }
+
+    public void onKlubovi(View view){
+        TextView klubovi = (TextView) findViewById(R.id.textViewKlubovi);
+        Log.i("Tu sam", "Kliknu sam na Klubovi");
+    }
+
+    public void onKviz(View view){
+        TextView kviz = (TextView) findViewById(R.id.textViewKviz);
+        Log.i("Tu sam", "Kliknu sam na Kviz");
     }
 
     @Override
